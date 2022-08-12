d09e11dc82f7ea07f6bb8085e235e2c3fd373d22
Fred Jiang <jiangf@shinetechsoftware.com>
2022-08-12 13:28:51 +0800

    add ComboBox

---
+++ PlayWpf/PlayWpf/Core/Converter/SingleValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PlayWpf.Core.Converter
+{
+    public class SingleValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string[] parray = parameter.ToString().Split(':');
+            if (value == null)
+            {
+                return null;
+            }
+
+            return parray[0].Equals(value.ToString(), StringComparison.InvariantCultureIgnoreCase) ? parray[1] : parray[2];  //单值比较
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var returnValue = "otherValue";
+            string[] parray = parameter.ToString().Split(':');
+            if (value == null)
+                return returnValue;
+            var valueStr = value.ToString();
+            if (valueStr != parray[1])
+                return returnValue;
+            else
+                return parray[0].Contains('|') ? parray[0].Split('|')[0] : parray[0];
+        }
+    }
+}
---
+++ PlayWpf/PlayWpf/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Ports;
+
+namespace PlayWpf.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            var currentMachiePorts = SerialPort.GetPortNames();
+            foreach (var item in currentMachiePorts)
+            {
+                this.PortList.Add(item);
+            }
+        }
+        
+        public ObservableCollection<string> PortList { get; set; } = new ObservableCollection<string> { "USB" };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
