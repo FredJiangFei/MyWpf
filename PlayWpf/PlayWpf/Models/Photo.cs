3355dc7667e1e3bd47a80bf14b0db4a41086954c
Fred Jiang <jiangf@shinetechsoftware.com>
2022-08-12 11:11:21 +0800

    add tab

---
+++ PlayWpf/PlayWpf/Models/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWpf.Models
+{
+    public class Photo
+    {
+        public Photo(string path)
+        {
+            Source = path;
+        }
+
+        public string Source { get; }
+
+        public override string ToString() => Source;
+    }
+}
